32a6a4ffb78ef72b2f55dca30a68cb6db6c07c5c
rug <rmfgoncalves@gmail.com>
2023-08-23 23:44:08 +0200

    Update ReactSelect.RenderSelect.js

--- demo2/OutSystems/ReactSelect.RenderSelect.js
+++ demo2/OutSystems/ReactSelect.RenderSelect.js
@@ -1,4 +1,11 @@
 $parameters.ReactRoot.render(
-    React.createElement(SelectWrapper, { options: JSON.parse($parameters.OptionsList), onChange: (option)=>$actions.OnChangeReact(JSON.stringify(option)) })
+    React.createElement(
+        SelectWrapper, 
+        { 
+            options: JSON.parse($parameters.OptionsList), 
+            onChange: (option)=>{
+                $actions.OnChangeReact(JSON.stringify(option));
+            } 
+        }
+    )
 );
-
